dfb30e43ef6ebd840348723cf86822ca4f30547b
Eros Nolasco-Mendoza <erosnolasco@me.com>
2023-10-18 12:35:05 -0700

    second commit, updated cs

--- HelloFromCSharp/Program.cs
+++ HelloFromCSharp/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+Console.WriteLine("Please type your first name: ");
+
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please type in your Last Name: ");
+
+string lastName = Console.ReadLine();
+
+Console.WriteLine($"Thank you, {firstName} {lastName}!");
+
+
+
+
